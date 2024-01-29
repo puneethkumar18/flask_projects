b10c79bbc4cc9ab55b5fce298e8d1782b26a71dc
Puneethkumar <puneethkumarg96@gmail.com>
2024-01-29 12:04:03 +0530

    enviornment setup

--- flask/app.py
+++ flask/app.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template,request,flash
+from flask import Flask, render_template,request,flash,jsonify
 app=Flask(__name__)
 
 @app.route("/hello")
 def index():
     return render_template("index.html")
 
+@app.route("/")
+def json():
+    return jsonify()
+
+
+
 if __name__==__name__:     
-    app.run(debug=True)
+    app.run()
