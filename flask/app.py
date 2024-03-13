ff4ee789d8d6d3e55dcf43384e079ad536e24bbd
PUNEETH KUMAR <119596819+puneethkumar18@users.noreply.github.com>
2024-03-13 20:41:44 +0530

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template,request,flash,jsonify
+
 app=Flask(__name__)
+
 
 @app.route("/hello")
 def index():
     return render_template("index.html")
 
+
+
 @app.route("/<puneeth>")
 def  send():
     return jsonify()
-
 
 
 if __name__==__name__:     
